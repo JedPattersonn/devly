80ab301b7e065a3d9069110fee9e396589456d84
Jed Patterson <jedpatterson123@gmail.com>
2024-07-20 14:34:58 +0100

    feat: :mag: Add Sitemap, Robots, update metadata
    Add sitemap.xml, robots.txt, update the metadata for the homepage

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://wwww.devly.dev",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://wwww.devly.dev/package-analyzer",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://wwww.devly.dev/package-analyzer/api",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://wwww.devly.dev/sitemap-generator",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
